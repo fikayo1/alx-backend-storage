9f8c45322d2b6e39ca035d21b41201ba6f82bc2d
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-11 06:25:18 +0100

    Added get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ Redis implementation with redis-py
 import redis
 import uuid
 from typing import Union
+from typing import Callable
 
 class Cache():
     """ A redis client instance"""
@@ -18,3 +19,20 @@ class Cache():
         key = uuid.uuid4()
         self._redis.set(str(key), data)
         return str(key)
+
+    def get(self, key: str, fn: Union[Callable, None]) -> Union[int, float, str, bytes, None]:
+        """A method to retrieve data from redis"""
+        if fn is None:
+            return self._redis.get(key)
+        return fn(self._redis.get(key))
+
+
+    def get_int(self, key: str) -> int:
+        """Get an integer from the cache."""
+        return int(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """Get a string from the cache."""
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+        
